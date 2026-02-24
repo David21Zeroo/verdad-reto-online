74c01cb56cb1a5eed9df6d94ff0a4a4f7e049f7a
David21Zeroo <165188537+David21Zeroo@users.noreply.github.com>
2026-02-24 18:31:29 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+const rooms = {};
+
+function generateCode() {
+  return Math.random().toString(36).substring(2, 7).toUpperCase();
+}
+
+io.on("connection", (socket) => {
+
+  socket.on("createRoom", (callback) => {
+    const code = generateCode();
+    rooms[code] = { players: [], turn: 0 };
+    socket.join(code);
+    callback(code);
+  });
+
+  socket.on("joinRoom", ({ code, name }, callback) => {
+    if (!rooms[code]) return callback("Sala no existe");
+    rooms[code].players.push({ id: socket.id, name });
+    socket.join(code);
+    io.to(code).emit("updateRoom", rooms[code]);
+    callback("OK");
+  });
+
+  socket.on("nextTurn", (code) => {
+    if (!rooms[code]) return;
+    rooms[code].turn =
+      (rooms[code].turn + 1) % rooms[code].players.length;
+    io.to(code).emit("updateRoom", rooms[code]);
+  });
+
+});
+
+server.listen(3000, () => console.log("Server running"));
