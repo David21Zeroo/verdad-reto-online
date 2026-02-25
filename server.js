4a32c3f2279c434dadd25eb8079d8d9ea96e7af4
David21Zeroo <165188537+David21Zeroo@users.noreply.github.com>
2026-02-24 19:50:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: { origin: "*" }
 });
@@ -14,33 +15,46 @@ const io = new Server(server, {
 const rooms = {};
 
 function generateCode() {
-  return Math.random().toString(36).substring(2, 7).toUpperCase();
+  return Math.random().toString(36).substring(2, 6).toUpperCase();
 }
 
 io.on("connection", (socket) => {
 
-  socket.on("createRoom", (callback) => {
+  socket.on("createRoom", ({ name }, callback) => {
+
     const code = generateCode();
-    rooms[code] = { players: [], turn: 0 };
+
+    rooms[code] = {
+      players: [{ id: socket.id, name }],
+      turn: 0
+    };
+
     socket.join(code);
     callback(code);
+    io.to(code).emit("updateRoom", rooms[code]);
   });
 
   socket.on("joinRoom", ({ code, name }, callback) => {
+
     if (!rooms[code]) return callback("Sala no existe");
+
     rooms[code].players.push({ id: socket.id, name });
     socket.join(code);
+
     io.to(code).emit("updateRoom", rooms[code]);
     callback("OK");
   });
 
   socket.on("nextTurn", (code) => {
+
     if (!rooms[code]) return;
+
     rooms[code].turn =
       (rooms[code].turn + 1) % rooms[code].players.length;
+
     io.to(code).emit("updateRoom", rooms[code]);
   });
 
 });
 
-server.listen(3000, () => console.log("Server running"));
+server.listen(3000, () => console.log("Servidor activo"));
